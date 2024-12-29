2b69606535a16b08e03138ad4f99b5acb1bf8aa2
menna <94606987+Menna120@users.noreply.github.com>
2024-12-29 15:07:33 +0200

    add splash screen theme

--- app/src/main/java/com/elfeky/devdash/MainActivity.kt
+++ app/src/main/java/com/elfeky/devdash/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.elfeky.devdash.navigation.app_navigation.AppNavigation
 import com.elfeky.devdash.ui.theme.DevDashTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             DevDashTheme {
