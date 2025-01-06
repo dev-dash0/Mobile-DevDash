6a6a296ec9464974d005a00e6a686674209b5963
menna <94606987+Menna120@users.noreply.github.com>
2025-01-06 22:03:13 +0200

    make one stander text field

---
+++ app/src/main/java/com/elfeky/devdash/ui/common/CustomTextField.kt
@@ -0,0 +1,129 @@
+package com.elfeky.devdash.ui.common
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import com.elfeky.devdash.ui.theme.DevDashTheme
+import com.elfeky.devdash.ui.theme.Gray
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    isPassword: Boolean = false,
+    isEmail: Boolean = false,
+    maxLines: Int = 1,
+    imeAction: ImeAction = ImeAction.Next,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null
+) {
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = label, style = MaterialTheme.typography.bodyLarge) },
+        singleLine = true,
+        modifier = modifier,
+        visualTransformation = if (isPassword && !passwordVisible) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = when {
+                isPassword -> KeyboardType.Password
+                isEmail -> KeyboardType.Email
+                else -> KeyboardType.Text
+            },
+            imeAction = imeAction
+        ),
+        trailingIcon = trailingIcon ?: if (isPassword) {
+            {
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(
+                        imageVector = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                        contentDescription = if (passwordVisible) "Hide password" else "Show password",
+                        tint = Gray
+                    )
+                }
+            }
+        } else null,
+        supportingText = supportingText,
+        maxLines = maxLines,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = MaterialTheme.colorScheme.onBackground,
+            unfocusedContainerColor = Color.White,
+            focusedContainerColor = Color.White,
+            focusedLabelColor = MaterialTheme.colorScheme.onBackground,
+        ),
+        shape = MaterialTheme.shapes.medium,
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PasswordTextFieldPreview(modifier: Modifier = Modifier) {
+    DevDashTheme {
+        var passwordValue by remember { mutableStateOf("") }
+        CustomTextField(
+            value = passwordValue,
+            onValueChange = { passwordValue = it },
+            label = "Password",
+            isPassword = true
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmailTextFieldPreview(modifier: Modifier = Modifier) {
+    DevDashTheme {
+        var emailValue by remember { mutableStateOf("") }
+        CustomTextField(
+            value = emailValue,
+            onValueChange = { emailValue = it },
+            label = "Email",
+            isEmail = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email",
+                    tint = Gray
+                )
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NormalTextFieldPreview(modifier: Modifier = Modifier) {
+    DevDashTheme {
+        var normalTextValue by remember { mutableStateOf("") }
+        CustomTextField(
+            value = normalTextValue,
+            onValueChange = { normalTextValue = it },
+            label = "Name",
+        )
+    }
+}
