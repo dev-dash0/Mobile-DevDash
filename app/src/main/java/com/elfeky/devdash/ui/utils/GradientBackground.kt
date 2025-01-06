1f3256b88c1f831b3ed562cc1983108eb357f527
menna <94606987+Menna120@users.noreply.github.com>
2025-01-06 22:02:39 +0200

    gradient background

---
+++ app/src/main/java/com/elfeky/devdash/ui/utils/GradientBackground.kt
@@ -0,0 +1,12 @@
+package com.elfeky.devdash.ui.utils
+
+import androidx.compose.ui.graphics.Brush
+import com.elfeky.devdash.ui.theme.Black
+import com.elfeky.devdash.ui.theme.NavyBlue
+
+val gradientBackground = Brush.linearGradient(
+    colors = listOf(
+        Black,
+        NavyBlue
+    )
+)
