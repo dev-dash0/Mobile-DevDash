eb96c1a44f58b7932faaa51e46611a1e61d33dc4
menna <94606987+Menna120@users.noreply.github.com>
2025-01-06 22:03:31 +0200

    a global button

---
+++ app/src/main/java/com/elfeky/devdash/ui/common/CustomButton.kt
@@ -0,0 +1,45 @@
+package com.elfeky.devdash.ui.common
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import com.elfeky.devdash.ui.utils.secondaryButtonColor
+
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit,
+    buttonColor: ButtonColors,
+    shape: Shape = MaterialTheme.shapes.small,
+    textColor: Color = Color.White,
+    textStyle: TextStyle = MaterialTheme.typography.titleMedium,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        colors = buttonColor
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            style = textStyle
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CustomTextButtonPreview() {
+    CustomButton("Create", {}, secondaryButtonColor)
+}
