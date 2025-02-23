6cc277cca6b687a1c935891fc833227456658f6f
Youssef_Elfeky <youssefelfeky83@gmail.com>
2025-02-23 21:12:37 +0200

    Setup hilt

--- app/src/main/java/com/elfeky/devdash/MainActivity.kt
+++ app/src/main/java/com/elfeky/devdash/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.elfeky.devdash.navigation.app_navigation.AppNavigation
 import com.elfeky.devdash.ui.theme.DevDashTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
