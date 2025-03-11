554064cee106dc042d77998778743c0f6672daac
menna <94606987+Menna120@users.noreply.github.com>
2025-03-11 15:33:52 +0200

    fix: the start destination after the splash screen

--- app/src/main/java/com/elfeky/devdash/ui/main_activity/MainActivity.kt
+++ app/src/main/java/com/elfeky/devdash/ui/main_activity/MainActivity.kt
@@ -16,16 +16,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.elfeky.devdash.navigation.app_navigation.AppNavigation
-import com.elfeky.devdash.navigation.app_navigation.AppScreen
 import com.elfeky.devdash.ui.theme.DevDashTheme
 import com.elfeky.devdash.ui.utils.gradientBackground
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -33,37 +37,32 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
 
-        val mainViewModel: MainViewModel by viewModels()
-
         setContent {
-            val appState by mainViewModel.appState.collectAsStateWithLifecycle()
+            var startDestination by remember { mutableStateOf<String?>(null) }
+            var keepSplashScreenVisible by remember { mutableStateOf(true) }
 
-            var startDestination by remember { mutableStateOf(AppScreen.MainScreen.route) }
-
-            var keepSplashScreen by remember { mutableStateOf(true) }
-
-            splashScreen.setKeepOnScreenCondition { keepSplashScreen }
-
-            DevDashTheme {
-                LaunchedEffect(appState) {
-                    when (appState) {
-                        is AppState.Unauthenticated -> {
-                            startDestination =
-                                (appState as AppState.Unauthenticated).startDestination
-                            delay(100)
-                            keepSplashScreen = false
+            LaunchedEffect(key1 = Unit) {
+                lifecycleScope.launch {
+                    repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        mainViewModel.checkAuthenticationStatus().collectLatest { destination ->
+                            startDestination = destination
+                            keepSplashScreenVisible = false
                         }
-
-                        else -> keepSplashScreen = false
                     }
                 }
+            }
 
-                AppNavigation(
-                    startDestination = startDestination,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(gradientBackground)
-                )
+            splashScreen.setKeepOnScreenCondition { keepSplashScreenVisible }
+
+            if (startDestination != null) {
+                DevDashTheme {
+                    AppNavigation(
+                        startDestination = startDestination!!,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(gradientBackground)
+                    )
+                }
             }
         }
     }
