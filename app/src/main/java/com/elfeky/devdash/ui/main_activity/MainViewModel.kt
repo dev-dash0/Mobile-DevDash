9a8920ddbb4e8b95184f2346878fdb1e817ffd5a
menna <94606987+Menna120@users.noreply.github.com>
2025-03-12 16:02:09 +0200

    fix : start destination

--- app/src/main/java/com/elfeky/devdash/ui/main_activity/MainViewModel.kt
+++ app/src/main/java/com/elfeky/devdash/ui/main_activity/MainViewModel.kt
@@ -1,38 +1,18 @@
 package com.elfeky.devdash.ui.main_activity
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.elfeky.devdash.navigation.app_navigation.AppScreen
 import com.elfeky.domain.usecase.local_storage.AccessTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     accessTokenUseCase: AccessTokenUseCase
 ) : ViewModel() {
-    var appState = MutableStateFlow<AppState>(AppState.Loading)
-        private set
+    private val accessToken: String? = accessTokenUseCase.get()
 
-    private val accessToken = accessTokenUseCase.get()
-
-    init {
-        checkAuthenticationStatus()
-    }
-
-    private fun checkAuthenticationStatus() {
-        viewModelScope.launch(Dispatchers.IO) {
-            appState.update {
-                if (accessToken.isNullOrEmpty()) {
-                    AppState.Unauthenticated(AppScreen.SignInScreen.route)
-                } else {
-                    AppState.Authenticated(AppScreen.MainScreen.route)
-                }
-            }
-        }
-    }
+    fun getStartDestination(): String =
+        if (accessToken.isNullOrEmpty()) AppScreen.SignInScreen.route
+        else AppScreen.MainScreen.route
 }
