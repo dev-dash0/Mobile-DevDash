8bf6cb5a8a739e542a71f42f94b3c1d4e9ad226a
menna <94606987+Menna120@users.noreply.github.com>
2025-05-19 12:20:43 +0300

    refactor: Simplify SharedPreferences operations using gson

--- domain/src/main/java/com/elfeky/domain/usecase/local_storage/IsFirstLoginUseCase.kt
+++ domain/src/main/java/com/elfeky/domain/usecase/local_storage/IsFirstLoginUseCase.kt
@@ -1,14 +1,16 @@
 package com.elfeky.domain.usecase.local_storage
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.elfeky.domain.repo.SharedPreferencesRepo
 import com.elfeky.domain.util.Constants.IS_FIRST_LOGIN
+import javax.inject.Inject
 
-class IsFirstLoginUseCase(private val sharedPref: SharedPreferences) :
+class IsFirstLoginUseCase @Inject constructor(private val sharedPref: SharedPreferences) :
     SharedPreferencesRepo<Boolean> {
-    override fun save(data: Boolean) = sharedPref.edit().putBoolean(IS_FIRST_LOGIN, data).apply()
+    override fun save(data: Boolean) = sharedPref.edit { putBoolean(IS_FIRST_LOGIN, data) }
 
     override fun get(): Boolean = sharedPref.getBoolean(IS_FIRST_LOGIN, true)
 
-    override fun delete() = sharedPref.edit().remove(IS_FIRST_LOGIN).apply()
+    override fun delete() = sharedPref.edit { remove(IS_FIRST_LOGIN) }
 }
--- domain/src/main/java/com/elfeky/domain/usecase/local_storage/RefreshTokenUseCase.kt
+++ domain/src/main/java/com/elfeky/domain/usecase/local_storage/RefreshTokenUseCase.kt
@@ -1,15 +1,17 @@
 package com.elfeky.domain.usecase.local_storage
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.elfeky.domain.repo.SharedPreferencesRepo
 import com.elfeky.domain.util.Constants.REFRESH_TOKEN_KEY
+import javax.inject.Inject
 
-class RefreshTokenUseCase(private val sharedPref: SharedPreferences) :
+class RefreshTokenUseCase @Inject constructor(private val sharedPref: SharedPreferences) :
     SharedPreferencesRepo<String> {
     override fun save(data: String) =
-        sharedPref.edit().putString(REFRESH_TOKEN_KEY, data).apply()
+        sharedPref.edit { putString(REFRESH_TOKEN_KEY, data) }
 
     override fun get(): String = sharedPref.getString(REFRESH_TOKEN_KEY, "") ?: ""
 
-    override fun delete() = sharedPref.edit().remove(REFRESH_TOKEN_KEY).apply()
+    override fun delete() = sharedPref.edit { remove(REFRESH_TOKEN_KEY) }
 }
