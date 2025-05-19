db5972d2d9d2436ff90af3f821e77c790abb992c
menna <94606987+Menna120@users.noreply.github.com>
2025-05-19 15:40:58 +0300

    refactor: Update user data model and composable arguments

--- app/src/main/java/com/elfeky/devdash/ui/common/tab_row/TabRowContainer.kt
+++ app/src/main/java/com/elfeky/devdash/ui/common/tab_row/TabRowContainer.kt
@@ -1,65 +1,45 @@
 package com.elfeky.devdash.ui.common.tab_row
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerScope
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabPosition
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.zIndex
+import androidx.compose.ui.tooling.preview.Preview
+import com.elfeky.devdash.ui.theme.DevDashTheme
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TabRowContainer(
+    tabs: List<String>,
     modifier: Modifier = Modifier,
     pageContent: @Composable (PagerScope.(page: Int) -> Unit)
 ) {
-    val tabItems = listOf("Board", "Sprint", "Timeline")
     val scope = rememberCoroutineScope()
-    val pagerState = rememberPagerState(pageCount = { tabItems.size })
+    val pagerState = rememberPagerState(pageCount = { tabs.size })
 
     Column(
-        modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TabRow(
+        CustomTabRow(
+            tabs = tabs,
             selectedTabIndex = pagerState.currentPage,
-            containerColor = Color.Transparent,
-            contentColor = MaterialTheme.colorScheme.onBackground,
-            indicator = { tabPositions: List<TabPosition> ->
-                if (pagerState.currentPage < tabPositions.size) {
-                    TabRowDefaults.PrimaryIndicator(
-                        Modifier.tabIndicatorOffset(currentTabPosition = tabPositions[pagerState.currentPage]),
-                        color = MaterialTheme.colorScheme.onBackground
-                    )
-                }
-            },
-            divider = {}
-        ) {
-            tabItems.forEachIndexed { index, item ->
-                Tab(
-                    modifier = Modifier.zIndex(2f),
-                    selected = index == pagerState.currentPage,
-                    onClick = { scope.launch { pagerState.animateScrollToPage(index) } },
-                    text = { Text(text = item) },
-                    selectedContentColor = MaterialTheme.colorScheme.onBackground,
-                    unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant
-                )
+            onTabClick = { index ->
+                scope.launch { pagerState.animateScrollToPage(index) }
             }
-        }
+        )
 
         HorizontalPager(
             state = pagerState,
@@ -68,5 +48,22 @@ fun TabRowContainer(
                 .weight(1f),
             pageContent = pageContent
         )
+    }
+}
+
+@Preview
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun TabRowContainerPreview() {
+    val sampleTabs = listOf("Tab 1", "Tab 2", "Tab 3")
+    DevDashTheme {
+        TabRowContainer(tabs = sampleTabs) { page ->
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Content for ${sampleTabs[page]}")
+            }
+        }
     }
 }
